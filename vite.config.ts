c32be468dfed287e6773e7e3180438204fc59b2f
tuhs1985 <77306309+tuhs1985@users.noreply.github.com>
2025-06-24 01:41:04 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pf2e-earn-income/', // 👈 important for GitHub Pages
+  base: '/', // ← this is the fix
   plugins: [react()],
 });
