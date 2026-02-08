f83a0c1ae1d2ed25be32146b2dfef68198c48084
tuhs1985 <77306309+tuhs1985@users.noreply.github.com>
2026-02-08 16:27:26 -0500

    updated .css

--- src/App.tsx
+++ src/App.tsx
@@ -364,7 +364,7 @@ export default function App() {
           <pre className="output-pre">{output}</pre>
         )}
 
-        <footer>
+          <footer className="footer">
           <a
             href="https://github.com/tuhs1985/pf2e-earn-income"
             target="_blank"
@@ -372,7 +372,20 @@ export default function App() {
           >
             View on GitHub / Report Issues
           </a>
-        </footer>
+		  
+          <p className="paizo-notice">
+            This website uses trademarks and/or copyrights owned by Paizo Inc., used under Paizo's Community Use Policy (paizo.com/licenses/communityuse). 
+            We are expressly prohibited from charging you to use or access this content. This website is not published, endorsed, or specifically approved by Paizo. 
+            For more information about Paizo Inc. and Paizo products, visit{' '}
+            <a 
+              href="https://paizo.com/" 
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              paizo.com
+            </a>.
+          </p>
+          </footer>
       </div>
     </div>
   );
